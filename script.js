9635cf5f163eb9025a6590c7849861ca4ceeec40
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-01-24 21:29:17 +0330

    Add event listener on submiting form and toggle form input
    and Add some additional CSS3 styles

--- script.js
+++ script.js
@@ -11,14 +11,15 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
@@ -30,22 +31,11 @@ if (navigator.geolocation)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
 
+      // Handling Click on map
       map.on('click', function (mapE) {
-        const { lat, lng } = mapE.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 50,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
-          
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
@@ -57,3 +47,34 @@ if (navigator.geolocation)
       maximumAge: 0,
     }
   );
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  // Clear form
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  // Display Marker after submition
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 50,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
